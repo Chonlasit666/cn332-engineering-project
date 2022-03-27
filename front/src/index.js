20a7b3eed1c622f91d65b90e5465eaeb9496f0d0
Ittipat purathep <ittipatpoon2@gmail.com>
2022-03-27 23:48:20 +0700

    update frontend navbar and routing

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TaskManage from './components/TaskManage';
+import AppNav from './components/AppNav';
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppNav />
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/Myprojecct" element={<TaskManage />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+    
+    
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ front/src/pages/Profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row } from "reactstrap";
+
+function Profile() {
+    return (
+        <Container>
+            <div>
+                <Row>
+                    <h1>3</h1>
+                    <h1>3</h1>
+                    <h1>3</h1>
+                </Row>
+                <Row>
+                    <h1>4</h1>
+                    <h1>4</h1>
+                    <h1>4</h1>
+                </Row>
+                <Row>
+                    <h1>5</h1>
+                    <h1>5</h1>
+                    <h1>5</h1>
+                </Row>
+            </div>
+        </Container>
+    )
+}
+export default Profile;
