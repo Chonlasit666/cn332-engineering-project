39950d0cb2148a6b13be4f2a52f9f88519d1d47c
raned.chu <raned.chu@gmail.com>
2022-03-30 00:39:48 +0700

    already done profile database and it signal

--- cn332project/rest_google_auth/admin.py
+++ cn332project/rest_google_auth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Profile
+
+
+class projectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('owner', 'adviser',)
+
+
+admin.site.register(Project, projectAdmin)
+admin.site.register(Profile)
--- cn332project/rest_google_auth/apps.py
+++ cn332project/rest_google_auth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RestGoogleAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rest_google_auth'
+
+    def ready(self):
+        import rest_google_auth.signals
