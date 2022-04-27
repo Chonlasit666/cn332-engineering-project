354872b5f536abfea3c7bcd5d95437a23e962374
raned.chu <raned.chu@gmail.com>
2022-04-27 21:39:28 +0700

    create models

--- server/features/admin.py
+++ server/features/admin.py
@@ -5,7 +5,9 @@ from .models import *
 class projectAdmin(admin.ModelAdmin):
     filter_horizontal = ('owner', 'adviser',)
 
-
+    
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Progressions)
+admin.site.register(Review)
 admin.site.register(Project, projectAdmin)
