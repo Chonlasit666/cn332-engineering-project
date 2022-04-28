33ff229cc83404a07762badf3132808fc2ee770e
ittip <ittipatpoon2@gmail.com>
2022-04-28 21:25:42 +0700

    edit project detail

--- client/src/pages/Project/index.js
+++ client/src/pages/Project/index.js
@@ -1,200 +1,83 @@
-import React from "react";
-import {
-    Form,
-    Container,
-} from "react-bootstrap";
+
+import { Container, Row, Col, Button, Image } from 'react-bootstrap'
+import React, { useContext, useCallback, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { getUser, getFeature, postUserm, putUser } from "../../utils/sdk";
+
+
+import { storage } from '../../utils/firebase';
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
+
+import { LOGIN_URL } from "../../config/urls";
+import { useUserRequired } from "../../utils/hooks";
+import { MiniProfile, Schedule, UserContext } from "../../components";
+
+import { logout, todo, updatetodo } from "../../pages/Home/sdk";
+
+/* must contain 
+        progress manage,
+        project detail,
+
+        have 2 type permission
+        1. owner -> add progress
+        2. advisor -> comment progress
+        3. visiter -> none of above
+
+
+*/
+
+//const updateProfile = (sample) => putUser("users/testPUT/", sample);
+const get_project = (project_id) => getFeature("posts/1/");
+
 
 const Project = () => {
-    const { useState } = React;
-    const [step, setStep] = useState(0);
-    const [hideButton, setHideButton] = useState(0);
-    const [formData, setFormData] = useState({});
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
+    useUserRequired();
 
-        setFormData((values) => ({ ...values, [name]: value }));
+    const [progress, setProgress] = useState(0);
+    const [project, setProject] = useState();
+    const [url, setUrl] = useState("");
 
-    };
+    const history = useHistory();
+    const { user, setUser } = useContext(UserContext);
 
-    const next_step_form = step + 1;
+    useEffect(() => {
+        console.log("call useEff")
+        get_project(3).then((resp) => {
+            setProject(resp.data);
 
-    const StepForm = () => {
+        });
+    }, []);
 
-        if (step == 0) {
-
-            return (
-                <Login />);
-
-        } else if (step == 1) {
-
-            return (
-                <Contact />);
-
-        } else if (step == 2) {
-
-            return (
-                <Social />);
-
-        } else if (step == 3) {
-
-            return (
-                <Welcome />);
-        }
-    }
-    const Login = () => {
-
-        return (
-            <Container className='justify-content-md-center'>
-                <div className="form_body">
-                    <div className="header">
-                        <h1>Multistep form</h1>
-                        <span>this is step{next_step_form}</span>
-                    </div>
-                    <div className="form_data">
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Name</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Email</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Password</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="password" required />
-                            <span>Repeat password</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button onClick={() => { setStep(step + 1) }}>Next</button>
-                </div>
-            </Container>
-
-
-        );
-    }
-
-
-    const Contact = () => {
-
-        return (
-            <>
-                <div className="form_body">
-                    <div className="header">
-                        <h1>Contact Info</h1>
-                        <span>{next_step_form}</span>
-                    </div>
-                    <div className="form_data">
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Country</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>State</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Address Line 1</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Address Line 2</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button onClick={() => { setStep(step - 1) }}>Previous</button>
-                    <button onClick={() => { setStep(step + 1) }}>Next</button>
-                </div>
-            </>
-        );
-    }
-
-
-    const Social = () => {
-
-        return (
-            <>
-                <div className="form_body">
-                    <div className="header">
-                        <h1>Social Info</h1>
-                        <span>{next_step_form}</span>
-                    </div>
-                    <div className="form_data">
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Facebook</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Instgram</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Linkedin</span>
-                        </div>
-
-                        <div className="input_field">
-                            <input type="text" required />
-                            <span>Twitter</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button onClick={() => { setStep(step - 1) }}>Previous</button>
-                    <button onClick={() => { setStep(step + 1) }}>Finish</button>
-                </div>
-            </>
-        );
-    }
-
-
-    const Welcome = () => {
-
-        return (
-            <>
-                <div className="final">
-                    <div className="final_content">
-                        <span className="check"><i className="fa fa-check"></i></span>
-                        <p>Your Information has been submitted! We will contact you soon!</p>
-                    </div>
-                </div>
-            </>
-        );
-    }
-
+    //console.log(project.id)
+    console.log(project)
 
 
 
     return (
-        <Container className='justify-content-md-center'>
-            <div class="form">
-                <div className="card">
-                    <div>{
-                        <StepForm />}</div>
 
-                </div>
-            </div>
+        <Container>
+            project
+           {/*  <Row>
+                <Col className="col-4column bg-white" md="4">
+                    <div className="profile-img">
+                        <Image src={user.avatar} a00lt="Logo" />
+
+                        <form onSubmit={formHandler}>
+                            <input type="file" className="input" />
+                            <button type="submit">Upload</button>
+                        </form>
+                        <hr />
+
+
+                    </div>
+                </Col >
+            </Row> */}
         </Container>
 
 
-    );
-
+    )
 }
 
 export default Project;
